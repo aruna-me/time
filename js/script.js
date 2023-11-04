289dc3d3a4fa8a7770edec1c29febab3eac05b76
aruna-me <128813102+aruna-me@users.noreply.github.com>
2023-11-04 07:44:32 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+setInterval (updateClock, 1000);
+
+let aniDelay = document.querySelector('.inner-1').style.animationDelay = '1800s';
+setTimeout(function() {aniDelay}, 1800000);
+
+
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    function hourFormat(format) {
+        if ( format > 12 ) {
+            let formatTime = format - 12;
+            return formatTime;
+        } else {
+            return format;
+        }
+    }
+
+    document.getElementById('hours').innerText = hourFormat(hours);
+    document.getElementById('minutes').innerText = minutes;
+
+    if ( minutes > 30 ) {
+        document.getElementById('minutes-under').innerText = ( minutes - 30 );
+    } else {
+        document.getElementById('minutes-under').innerText = minutes;
+    }
+
+
+    if ( minutes == 0 || minutes == 30 ) {
+
+        const innerOne = document.querySelector('.inner-1');
+        innerOne.classList.add('inner-one');
+        const innerTwo = document.querySelector('.inner-2');
+        innerTwo.classList.add('inner-two');
+
+    }
+
+}
